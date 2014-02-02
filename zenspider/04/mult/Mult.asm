e4de921f971d47812a6403debb1a1962244aa7d8
Ryan Davis <ryand-ruby@zenspider.com>
2014-02-01 20:34:00 -0800

    04/mult

--- zenspider/04/mult/Mult.asm
+++ zenspider/04/mult/Mult.asm
@@ -1,3 +1,4 @@
+// -*- text -*-
 // This file is part of www.nand2tetris.org
 // and the book "The Elements of Computing Systems"
 // by Nisan and Schocken, MIT Press.
@@ -7,3 +8,32 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[3], respectively.)
 
 // Put your code here.
+
+        @R2
+        M=0             // r2 = 0
+
+        @R1
+        D=M
+        @END
+        D;JLE           // skip if r1 == 0
+
+(LOOP)
+        @R1
+        D=M
+        @END
+        D;JLE           // while r1 > 0
+
+        @R0
+        D=M
+        @R2
+        M=M+D           // r2 = r2 + r0
+
+        @R1
+        M=M-1           // r1 = r1 - 1
+
+        @LOOP
+        0;JMP
+
+(END)
+        @END
+        0;JMP
