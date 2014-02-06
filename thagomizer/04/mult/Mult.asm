e2dab41cfc84be172aa9d46a582abf8353c9d503
Aja Hammerly <aja.hammerly+github@gmail.com>
2014-02-05 20:22:13 -0800

    Multiplication in Hack What kind of assembly doesn't have mulitplication built in?

--- thagomizer/04/mult/Mult.asm
+++ thagomizer/04/mult/Mult.asm
@@ -6,4 +6,27 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[3], respectively.)
 
-// Put your code here.
+
+   @R2     // R2 = 0
+   M=0
+
+(LOOP)
+   @R1     // If R1 < 0 GOTO END
+   D=M
+   @END
+   D;JLE
+
+   @R0     // R2 = R2 + R0
+   D = M
+   @R2
+   M = M + D
+
+   @R1     // R1 = R1 - 1
+   M = M - 1
+
+   @LOOP   // GOTO LOOP
+   0;JMP
+
+(END)
+   @END
+   0;JMP
