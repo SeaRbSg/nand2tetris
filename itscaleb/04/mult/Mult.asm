159636ba896e32e6ee17d9c069bbd0ca3016357e
Caleb Gossler <caleb.gossler@gmail.com>
2014-02-10 21:30:25 -0800

    Mult.asm test is passing

--- itscaleb/04/mult/Mult.asm
+++ itscaleb/04/mult/Mult.asm
@@ -7,3 +7,54 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[3], respectively.)
 
 // Put your code here.
+
+// Pseudo Code:
+// r2 = 0;
+// 
+// while(r1 > 0)
+// {
+//   r1 = r1 - 1;
+//   r2 = r2 + r0;
+// }
+
+// r2 = 0;
+	@R2
+	M=0
+
+// make sure r0 and r1 are not zero
+	@R0
+	D=M
+	@END
+	D;JEQ
+
+	@R1
+	D=M
+	@END
+	D;JEQ
+
+(MULT)
+//while(r1 > 0) {
+	@R1
+	D=M
+
+	@END
+	D;JEQ
+
+// r1 = r1 - 1
+	@R1
+	M=M-1
+
+// r2 = r2 + r0
+	@R0
+	D=M
+
+	@R2
+	M=M+D
+
+	@MULT
+	0;JMP
+// }
+
+(END)
+	@END
+	0;JMP
