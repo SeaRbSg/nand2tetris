e4ecce8e0796762204b3c79b4190ca16a40cb33f
Pete Higgins <pete@peterhiggins.org>
2014-02-16 13:41:08 -0800

    Mult.

--- phiggins/04/mult/Mult.asm
+++ phiggins/04/mult/Mult.asm
@@ -6,4 +6,29 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[3], respectively.)
 
-// Put your code here.
+@R2
+M=0
+
+(LOOP)
+// Check loop condition. Quit if R0 <= 0.
+@R0
+D=M
+@END
+D;JLE
+
+// Update loop condition.
+@R0
+M=M-1
+
+// Do the repeated addition.
+@R1
+D=M
+@R2
+M=D+M
+
+@LOOP
+0;JMP
+
+(END)
+@END
+0;JMP
