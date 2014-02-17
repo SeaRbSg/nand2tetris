1652c2fdc3ecca2a94d4243fbef93257025dad45
Fernando Paredes <nano@fdp.io>
2014-02-17 11:24:08 -0800

    First attempt at Multiplication

--- nanoxd/04/mult/Mult.asm
+++ nanoxd/04/mult/Mult.asm
@@ -7,3 +7,30 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[3], respectively.)
 
 // Put your code here.
+
+  @R2
+  M=0
+
+// R1 <= 0 GOTO END
+(LOOP)
+  @R1
+  D=M
+  @END
+  D;JLE
+
+  @R0
+  D=M
+  @R2
+  M=M+D
+
+  // (R1 -1)
+  @R1
+  M=M+1
+
+  // Loopit
+  @LOOP
+  0;JMP
+
+(END)
+  @END
+  0;JMP
