30056eb18964560d2b06ff54c32e248b480c65be
James FitzGibbon <james.fitzgibbon@nsn.com>
2014-02-17 20:13:21 -0800

    Finish mult program (paired with Greg as only two people showed for Ballard group)

--- jf647/04/mult/Mult.asm
+++ jf647/04/mult/Mult.asm
@@ -6,4 +6,25 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[3], respectively.)
 
-// Put your code here.
+    @R2         // clear out R2 to be safe
+    M=0
+(LOOP)
+    @R1
+    D=M         // set D to the value of R1
+    @END
+    D;JEQ       // jump to @END if D(R1) is 0
+
+    @R0
+    D=M         // set D to the value of R0
+
+    @R2
+    M=D+M       // set D to the value of R0 + the value of R2
+
+    @R1
+    M=M-1       // decrement the value of R1 by 1
+
+    @LOOP
+    0;JMP       // jump to @LOOP unconditionally
+(END)
+    @END
+    0;JMP       // jump to @END unconditionally
