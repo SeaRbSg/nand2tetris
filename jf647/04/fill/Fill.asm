cabe9ee88b36846e7d6216029b081e4041996b81
James FitzGibbon <james.fitzgibbon@nsn.com>
2014-02-22 08:50:48 -0800

    Finish first working version of Fill

--- jf647/04/fill/Fill.asm
+++ jf647/04/fill/Fill.asm
@@ -1,13 +1,19 @@
-// R0 points the the current block of video RAM to operate on
-// 16384 <= R0 => 24575
-
-    // set R0 to 16384
+    // set R0 to 16384 (the first block of VRAM to operate on)
     @16384
     D=A
     @R0
     M=D
 
 (LOOP)
+    // branch to FILL or CLEAR based on whether a key is pressed
+    @24576
+    D=M
+    @FILL
+    D;JNE
+    @CLEAR
+    0;JMP
+
+(FILL)
     // set A to the value of R0 (the current VRAM block)
     @R0
     A=M
@@ -16,12 +22,12 @@
 
     // are we pointing at the last VRAM block?
     // i.e. is R0 == 24575?
-    // if so, jump to @END
+    // if so, just restart the loop
     @R0
     D=M
     @24575
     D=D-A
-    @END
+    @LOOP
     D;JEQ
 
     // otherwise increment R0
@@ -32,7 +38,27 @@
     @LOOP
     0;JEQ        // iterate
 
-(END)
-    // infinite loop back to @END to finish the program
-    @END
-    0;JEQ
+(CLEAR)
+    // set A to the value of R0 (the current VRAM block)
+    @R0
+    A=M
+    // setting a VRAM block to 0 will clear all the pixels of that block
+    M=0
+
+    // are we pointing at the first VRAM block?
+    // i.e. is R0 == 16384?
+    // if so, just restart the loop
+    @R0
+    D=M
+    @16384
+    D=D-A
+    @LOOP
+    D;JEQ
+
+    // otherwise decrement R0
+    @R0
+    M=M-1
+
+    // and go for another loop iteration
+    @LOOP
+    0;JEQ        // iterate
