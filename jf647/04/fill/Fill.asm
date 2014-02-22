13606895acbfcc93b40fe6118e9661e6bca21c4c
James FitzGibbon <james.fitzgibbon@nsn.com>
2014-02-22 08:00:15 -0800

    Trying this iteratively as I learn.
    This version just fills the screen regardless of keypress state (which is the easy part).  I'm filling one VRAM block at at time, but because you Hack can't load 16 bit unsigned values, I can only block the first 15 bits of every 16 bit block.
    
    Yay for pinstripes?

--- jf647/04/fill/Fill.asm
+++ jf647/04/fill/Fill.asm
@@ -1,11 +1,41 @@
-// This file is part of www.nand2tetris.org
-// and the book "The Elements of Computing Systems"
-// by Nisan and Schocken, MIT Press.
-// File name: projects/04/Fill.asm
+// R0 points the the current block of video RAM to operate on
+// 16384 <= R0 => 24575
 
-// Runs an infinite loop that listens to the keyboard input. 
-// When a key is pressed (any key), the program blackens the screen,
-// i.e. writes "black" in every pixel. When no key is pressed, the
-// program clears the screen, i.e. writes "white" in every pixel.
+    // set R0 to 16384
+    @16384
+    D=A
+    @R0
+    M=D
 
-// Put your code here.
+(LOOP)
+    // set D to 65535 (all bits of a VRAM block filled)
+    @32767
+    D=A
+    // set A to the value of R0 (the current VRAM block)
+    @R0
+    A=M
+    // set the current VRAM block to all bits filled
+    M=D
+
+    // are we pointing at the last VRAM block?
+    // i.e. is R0 == 24575?
+    // if so, jump to @END
+    @R0
+    D=M
+    @24575
+    D=D-A
+    @END
+    D;JEQ
+
+    // otherwise increment R0
+    @R0
+    M=M+1
+
+    // and go for another loop iteration
+    @LOOP
+    0;JEQ        // iterate
+
+(END)
+    // infinite loop back to @END to finish the program
+    @END
+    0;JEQ
