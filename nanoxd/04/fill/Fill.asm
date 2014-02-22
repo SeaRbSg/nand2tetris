4f32eb8367e1a7084c5e86a78e49ef44f672dd04
Fernando Paredes <nano@fdp.io>
2014-02-22 12:31:46 -0800

    Fill

--- nanoxd/04/fill/Fill.asm
+++ nanoxd/04/fill/Fill.asm
@@ -3,9 +3,58 @@
 // by Nisan and Schocken, MIT Press.
 // File name: projects/04/Fill.asm
 
-// Runs an infinite loop that listens to the keyboard input. 
+// Runs an infinite loop that listens to the keyboard input.
 // When a key is pressed (any key), the program blackens the screen,
 // i.e. writes "black" in every pixel. When no key is pressed, the
 // program clears the screen, i.e. writes "white" in every pixel.
 
 // Put your code here.
+
+(INIT)
+	@SCREEN
+	D=A
+	@screen_pointer
+	M=D
+
+	@8192
+	D=D+A
+	@SCREEN_END
+	M=D
+
+	// Set color based on input
+	@KBD
+	D=M
+	@BLANCO
+	D;JEQ
+
+	@color
+	M=-1
+
+	@DIBUJAR
+	0;JMP
+
+(BLANCO)
+	@color
+	M=0
+
+(DIBUJAR)
+	// Go back to init if in last screen memory map
+	@screen_pointer
+	D=M
+	@SCREEN_END
+	D=D-M
+	@INIT
+	D;JEQ
+
+	// Change memory to correct color
+	@color
+	D=M
+	@screen_pointer
+	A=M
+	M=D
+
+	@screen_pointer
+	M=M+1
+
+	@DIBUJAR
+	0;JMP
