8eae26bedd2eaac1484b365ad477b6f374dc79a9
Joel Stimson <stimsojr@gmail.com>
2014-02-23 15:11:17 -0800

    first attempt on Mult. Surprised I got it right the first time because I didn't know if I was calling the locations correctly

--- stim371/04/mult/Mult.asm
+++ stim371/04/mult/Mult.asm
@@ -7,3 +7,28 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[3], respectively.)
 
 // Put your code here.
+
+  @R2
+  M=0
+(LOOP)
+  // add R0 into Data
+  @R0
+  A=M
+
+  @R2
+  MD=D+A
+  
+  // decrement R1
+  @R1
+  M=M-1
+  
+  // if r1 == 0 JMP to END
+  @END
+  M;JEQ
+
+  // else start the loop over
+  @LOOP
+  0;JMP
+(END)
+  @END
+  0;JMP
