f144c2fbcd0f0b0d2c77e387f21edbed377ee0f4
Aja Hammerly <aja.hammerly+github@gmail.com>
2014-03-04 00:40:24 -0800

    Remove unnecessary methods

--- thagomizer/06/lib/code.rb
+++ thagomizer/06/lib/code.rb
@@ -1,117 +1,56 @@
 class Code
-  def self.dest(mnemonic)
-    case mnemonic
-    when nil
-      "000"
-    when "M"
-      "001"
-    when "D"
-      "010"
-    when "MD"
-      "011"
-    when "A"
-      "100"
-    when "AM"
-      "101"
-    when "AD"
-      "110"
-    when "AMD"
-      "111"
-    end
-  end
+  DEST = { nil   => "000",
+           "M"   => "001",
+           "D"   => "010",
+           "MD"  => "011",
+           "A"   => "100",
+           "AM"  => "101",
+           "AD"  => "110",
+           "AMD" => "111"}
 
-  def self.jump(mnemonic)
-    case mnemonic
-    when nil
-      "000"
-    when "JGT"
-      "001"
-    when "JEQ"
-      "010"
-    when "JGE"
-      "011"
-    when "JLT"
-      "100"
-    when "JNE"
-      "101"
-    when "JLE"
-      "110"
-    when "JMP"
-      "111"
-    end
-  end
+  JUMP = { nil  => "000",
+          "JGT" => "001",
+          "JEQ" => "010",
+          "JGE" => "011",
+          "JLT" => "100",
+          "JNE" => "101",
+          "JLE" => "110",
+          "JMP" => "111"}
 
-  def self.comp(mnemonic)
-    case mnemonic
-    when "0"
-      "0101010"
-    when "1"
-      "0111111"
-    when "-1"
-      "0111010"
-    when "D"
-      "0001100"
-    when "A"
-      "0110000"
-    when "M"
-      "1110000"
-    when "!D"
-      "0001101"
-    when "!A"
-      "0110001"
-    when "!M"
-      "1110001"
-    when "-D"
-      "0001111"
-    when "-A"
-      "0110011"
-    when "-M"
-      "1110011"
-    when "D+1"
-      "0011111"
-    when "A+1"
-      "0110111"
-    when "M+1"
-      "1110111"
-    when "D-1"
-      "0001110"
-    when "A-1"
-      "0110010"
-    when "M-1"
-      "1110010"
-    when "D+A"
-      "0000010"
-    when "D+M"
-      "1000010"
-    when "D-A"
-      "0010011"
-    when "D-M"
-      "1010011"
-    when "A-D"
-      "0000111"
-    when "M-D"
-      "1000111"
-    when "D&A"
-      "0000000"
-    when "D&M"
-      "1000000"
-    when "D|A"
-      "0010101"
-    when "D|M"
-      "1010101"
-    end
-  end
+  COMP = { "0"   => "0101010",
+           "1"   => "0111111",
+           "-1"  => "0111010",
+           "D"   => "0001100",
+           "A"   => "0110000",
+           "M"   => "1110000",
+           "!D"  => "0001101",
+           "!A"  => "0110001",
+           "!M"  => "1110001",
+           "-D"  => "0001111",
+           "-A"  => "0110011",
+           "-M"  => "1110011",
+           "D+1" => "0011111",
+           "A+1" => "0110111",
+           "M+1" => "1110111",
+           "D-1" => "0001110",
+           "A-1" => "0110010",
+           "M-1" => "1110010",
+           "D+A" => "0000010",
+           "D+M" => "1000010",
+           "D-A" => "0010011",
+           "D-M" => "1010011",
+           "A-D" => "0000111",
+           "M-D" => "1000111",
+           "D&A" => "0000000",
+           "D&M" => "1000000",
+           "D|A" => "0010101",
+           "D|M" => "1010101"}
 
   def self.c_command(dest, comp, jump)
-    inst = "111"
-
-    inst << Code.comp(comp)
-    inst << Code.dest(dest)
-    inst << Code.jump(jump)
+    "111#{COMP[comp]}#{DEST[dest]}#{JUMP[jump]}"
   end
 
   def self.a_command(literal)
-    inst = "0"
-    inst << literal.to_i.to_s(2).rjust(15, "0")
+    literal.to_i.to_s(2).rjust(16, "0")
   end
 end
--- thagomizer/06/test/test_code.rb
+++ thagomizer/06/test/test_code.rb
@@ -5,56 +5,56 @@ require_relative '../lib/code'
 class TestCode < Minitest::Test
 
   def test_dest
-    assert_equal "000", Code.dest(nil)
-    assert_equal "001", Code.dest("M")
-    assert_equal "010", Code.dest("D")
-    assert_equal "011", Code.dest("MD")
-    assert_equal "100", Code.dest("A")
-    assert_equal "101", Code.dest("AM")
-    assert_equal "110", Code.dest("AD")
-    assert_equal "111", Code.dest("AMD")
+    assert_equal "000", Code::DEST[nil]
+    assert_equal "001", Code::DEST["M"]
+    assert_equal "010", Code::DEST["D"]
+    assert_equal "011", Code::DEST["MD"]
+    assert_equal "100", Code::DEST["A"]
+    assert_equal "101", Code::DEST["AM"]
+    assert_equal "110", Code::DEST["AD"]
+    assert_equal "111", Code::DEST["AMD"]
   end
 
   def test_comp
-    assert_equal "0101010", Code.comp("0")
-    assert_equal "0111111", Code.comp("1")
-    assert_equal "0111010", Code.comp("-1")
-    assert_equal "0001100", Code.comp("D")
-    assert_equal "0110000", Code.comp("A")
-    assert_equal "1110000", Code.comp("M")
-    assert_equal "0001101", Code.comp("!D")
-    assert_equal "0110001", Code.comp("!A")
-    assert_equal "1110001", Code.comp("!M")
-    assert_equal "0001111", Code.comp("-D")
-    assert_equal "0110011", Code.comp("-A")
-    assert_equal "1110011", Code.comp("-M")
-    assert_equal "0011111", Code.comp("D+1")
-    assert_equal "0110111", Code.comp("A+1")
-    assert_equal "1110111", Code.comp("M+1")
-    assert_equal "0001110", Code.comp("D-1")
-    assert_equal "0110010", Code.comp("A-1")
-    assert_equal "1110010", Code.comp("M-1")
-    assert_equal "0000010", Code.comp("D+A")
-    assert_equal "1000010", Code.comp("D+M")
-    assert_equal "0010011", Code.comp("D-A")
-    assert_equal "1010011", Code.comp("D-M")
-    assert_equal "0000111", Code.comp("A-D")
-    assert_equal "1000111", Code.comp("M-D")
-    assert_equal "0000000", Code.comp("D&A")
-    assert_equal "1000000", Code.comp("D&M")
-    assert_equal "0010101", Code.comp("D|A")
-    assert_equal "1010101", Code.comp("D|M")
+    assert_equal "0101010", Code::COMP["0"]
+    assert_equal "0111111", Code::COMP["1"]
+    assert_equal "0111010", Code::COMP["-1"]
+    assert_equal "0001100", Code::COMP["D"]
+    assert_equal "0110000", Code::COMP["A"]
+    assert_equal "1110000", Code::COMP["M"]
+    assert_equal "0001101", Code::COMP["!D"]
+    assert_equal "0110001", Code::COMP["!A"]
+    assert_equal "1110001", Code::COMP["!M"]
+    assert_equal "0001111", Code::COMP["-D"]
+    assert_equal "0110011", Code::COMP["-A"]
+    assert_equal "1110011", Code::COMP["-M"]
+    assert_equal "0011111", Code::COMP["D+1"]
+    assert_equal "0110111", Code::COMP["A+1"]
+    assert_equal "1110111", Code::COMP["M+1"]
+    assert_equal "0001110", Code::COMP["D-1"]
+    assert_equal "0110010", Code::COMP["A-1"]
+    assert_equal "1110010", Code::COMP["M-1"]
+    assert_equal "0000010", Code::COMP["D+A"]
+    assert_equal "1000010", Code::COMP["D+M"]
+    assert_equal "0010011", Code::COMP["D-A"]
+    assert_equal "1010011", Code::COMP["D-M"]
+    assert_equal "0000111", Code::COMP["A-D"]
+    assert_equal "1000111", Code::COMP["M-D"]
+    assert_equal "0000000", Code::COMP["D&A"]
+    assert_equal "1000000", Code::COMP["D&M"]
+    assert_equal "0010101", Code::COMP["D|A"]
+    assert_equal "1010101", Code::COMP["D|M"]
   end
 
   def test_jump
-    assert_equal "000", Code.jump(nil)
-    assert_equal "001", Code.jump("JGT")
-    assert_equal "010", Code.jump("JEQ")
-    assert_equal "011", Code.jump("JGE")
-    assert_equal "100", Code.jump("JLT")
-    assert_equal "101", Code.jump("JNE")
-    assert_equal "110", Code.jump("JLE")
-    assert_equal "111", Code.jump("JMP")
+    assert_equal "000", Code::JUMP[nil]
+    assert_equal "001", Code::JUMP["JGT"]
+    assert_equal "010", Code::JUMP["JEQ"]
+    assert_equal "011", Code::JUMP["JGE"]
+    assert_equal "100", Code::JUMP["JLT"]
+    assert_equal "101", Code::JUMP["JNE"]
+    assert_equal "110", Code::JUMP["JLE"]
+    assert_equal "111", Code::JUMP["JMP"]
   end
 
   def test_c_command
