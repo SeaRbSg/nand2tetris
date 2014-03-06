95c654578b5b385020737826b691b67d99721a81
Aja Hammerly <aja.hammerly+github@gmail.com>
2014-03-05 22:25:22 -0800

    Really basic vm_translator

---
+++ thagomizer/07/lib/vm_translator.rb
@@ -0,0 +1,44 @@
+#!/usr/bin/env ruby -w
+require 'pp'
+
+require_relative 'parser'
+require_relative 'code_writer'
+require 'stringio'
+
+class VMTranslator
+
+  def initialize source
+    @parser = Parser.new(source)
+    @code_writer = CodeWriter.new
+  end
+
+  def self.run path
+    sio = StringIO.new File.read path
+
+    vmt = VMTranslator.new(sio)
+
+    vmt.translate
+    vmt.write(path.gsub(/\..*/, '.asm'))
+  end
+
+  def translate
+    while @parser.has_more_commands?
+      @parser.advance
+
+      case @parser.command_type
+      when :c_push
+        @code_writer.write_push(@parser.arg1, @parser.arg2)
+      when :c_pop
+        @code_writer.write_pop(@parser.arg1, @parser.arg2)
+      when :c_arithmetic
+        @code_writer.write_arithmetic(@parser.arg1)
+      end
+    end
+  end
+
+  def write path
+    @code_writer.write path
+  end
+end
+
+VMTranslator.run ARGV[0]
