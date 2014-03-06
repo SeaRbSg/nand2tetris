f710f1dd689302bac13237de60f748ecddd3c1eb
Aja Hammerly <aja.hammerly+github@gmail.com>
2014-03-05 22:25:36 -0800

    My manually calculated ASM and programmatically generated ASM

---
+++ thagomizer/07/StackArithmetic/SimpleAdd/SimpleAdd.asm
@@ -0,0 +1,29 @@
+// push constant 7
+@7
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// push constant 8
+@8
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+// add
+@SP
+AM=M-1
+D=M
+@SP
+AM=M-1
+A=M
+D=D+A
+@SP
+A=M
+M=D
+@SP
+M=M+1
