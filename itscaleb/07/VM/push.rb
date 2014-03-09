efb4f5b08f07a84892668a212b07c077171a0679
Caleb Gossler <caleb.gossler@gmail.com>
2014-03-09 15:26:16 -0700

    push test passing

---
+++ itscaleb/07/VM/push.rb
@@ -0,0 +1,21 @@
+class Push
+  def initialize segment, index
+    @segment = segment
+    @index = index
+  end
+
+  def to_asm
+    asm = []
+    asm << "D=A"
+    asm << "@#@index"
+    Push.push_d asm
+  end
+
+  def self.push_d asm
+    asm << "@SP"
+    asm << "A=M"
+    asm << "M=D"
+    asm << "@SP"
+    asm << "M=M+1"
+  end
+end
