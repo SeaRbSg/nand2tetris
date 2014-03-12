7b74b0fa7c86e575a3448ea645c3abc3f9a15c6f
Aja Hammerly <aja.hammerly+github@gmail.com>
2014-03-11 20:55:54 -0700

    Refactor from Ryan's feedback

--- thagomizer/08/lib/parser.rb
+++ thagomizer/08/lib/parser.rb
@@ -9,7 +9,7 @@ class Parser
   CALL_COMMAND     = /^call(?:\s+([\w.]+))?(?:\s+(\w+))?$/
   ARTH_COMMAND     = /^(add|sub|neg|eq|gt|lt|and|or|not)(?:\s+([\w.]+))?(?:\s+(\w+))?$/
 
-  attr_accessor :current_command, :arg1, :arg2
+  attr_accessor :current_command, :arg1, :arg2, :command_type
 
   def initialize source
     @source = source
@@ -25,34 +25,28 @@ class Parser
       @current_command = @source.gets.gsub(/(\/\/.*$)/, '').gsub(/\s*\n/, '').strip
     end while @current_command.empty?
 
-   command_type
-  end
-
-  def command_type
-    c_type = case @current_command
-             when PUSH_COMMAND
-               :c_push
-             when POP_COMMAND
-               :c_pop
-             when LABEL_COMMAND
-               :c_label
-             when GOTO_COMMAND
-               :c_goto
-             when IF_COMMAND
-               :c_if
-             when FUNCTION_COMMAND
-               :c_function
-             when RETURN_COMMAND
-               :c_return
-             when CALL_COMMAND
-               :c_call
-             when ARTH_COMMAND
-               :c_arithmetic
-             end
+    @command_type = case @current_command
+                    when PUSH_COMMAND
+                      :c_push
+                    when POP_COMMAND
+                      :c_pop
+                    when LABEL_COMMAND
+                      :c_label
+                    when GOTO_COMMAND
+                      :c_goto
+                    when IF_COMMAND
+                      :c_if
+                    when FUNCTION_COMMAND
+                      :c_function
+                    when RETURN_COMMAND
+                      :c_return
+                    when CALL_COMMAND
+                      :c_call
+                    when ARTH_COMMAND
+                      :c_arithmetic
+                    end
 
     @arg1, @arg2 = $~.captures
     @arg2 = @arg2.to_i
-
-    c_type
   end
 end
