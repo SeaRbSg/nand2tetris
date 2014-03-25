ba99dcb45db48272725fc0cdc937bce43e48faa9
Caleb Gossler <caleb.gossler@gmail.com>
2014-03-24 21:23:39 -0700

    making use of regex

--- itscaleb/08/VM/vm_to_asm.rb
+++ itscaleb/08/VM/vm_to_asm.rb
@@ -11,13 +11,11 @@ class Compiler
     asm = []
     lines.each do |line|
       case line
-      when /^push/
-        instructions = line.split
-        push = Push.new instructions[1], instructions[2].to_i, file
+      when /^push (\w+) (\d+)/
+        push = Push.new $1, $2.to_i, file
         asm << push.to_asm
-      when /^pop/
-        instructions = line.split
-        pop = Pop.new instructions[1], instructions[2].to_i, file
+      when /^pop (\w+) (\d+)/
+        pop = Pop.new $1, $2.to_i, file
         asm << pop.to_asm
       when /^add/
         asm << Add.to_asm
@@ -37,24 +35,19 @@ class Compiler
         asm << Or.to_asm
       when /^not/
         asm << Not.to_asm
-      when /^label/
-        instructions = line.split
-        asm << Label.to_asm(instructions[1])
-      when /^if-goto/
-        instructions = line.split
-        asm << IfGoto.to_asm(instructions[1])
-      when /^goto/
-        instructions = line.split
-        asm << Goto.to_asm(instructions[1])
-      when /^function/
-        instructions = line.split
-        func = Function.new(instructions[1], instructions[2].to_i)
+      when /^label (.+)/
+        asm << Label.to_asm($1)
+      when /^if-goto (.+)/
+        asm << IfGoto.to_asm($1)
+      when /^goto (.+)/
+        asm << Goto.to_asm($1)
+      when /^function (.+) (\d+)/
+        func = Function.new($1, $2.to_i)
         asm << func.to_asm
       when /^return/
         asm << Return.to_asm
-      when /^call/
-        instructions = line.split
-        call = Call.new(instructions[1], instructions[2].to_i)
+      when /^call (.+) (\d+)/
+        call = Call.new($1, $2.to_i)
         asm << call.to_asm
       else
         raise("cannot parse line: " + line)
@@ -73,13 +66,13 @@ vm_files.each do |vm_file|
     .select {|line| !line.start_with?("//") && !line.empty?}
     .map {|line| line.split("//")[0].strip}
 
-    asm << [
-      "@256",
-      "D=A",
-      "@SP",
-      "M=D"
-    ]
-    asm << Call.new("Sys.init", 0).to_asm
+    #asm << [
+    #  "@256",
+    #  "D=A",
+    #  "@SP",
+    #  "M=D"
+    #]
+    #asm << Call.new("Sys.init", 0).to_asm
     asm << Compiler.run(clean_lines, vm_file.split('/').last)
 end
 
