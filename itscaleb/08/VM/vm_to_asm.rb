f07c3d1e0f7bca3647482e206c5ae9777960502f
Caleb Gossler <caleb.gossler@gmail.com>
2014-03-24 22:12:44 -0700

    moving bootstrap code where it make a little more sense.

--- itscaleb/08/VM/vm_to_asm.rb
+++ itscaleb/08/VM/vm_to_asm.rb
@@ -7,15 +7,16 @@ require_relative 'program_flow'
 require_relative 'function_calling'
 
 class Compiler
-  def self.run lines, file
+  def self.run lines, filename
     asm = []
+    asm << Compiler.bootstrap_code
     lines.each do |line|
       case line
       when /^push (\w+) (\d+)/
-        push = Push.new $1, $2.to_i, file
+        push = Push.new $1, $2.to_i, filename
         asm << push.to_asm
       when /^pop (\w+) (\d+)/
-        pop = Pop.new $1, $2.to_i, file
+        pop = Pop.new $1, $2.to_i, filename
         asm << pop.to_asm
       when /^(add|eq|lt|gt|sub|neg|and|or|not)/
         operator = Object.const_get($1.capitalize)
@@ -40,6 +41,15 @@ class Compiler
     end
     asm
   end
+
+  def self.bootstrap_code
+    [ "@256",
+      "D=A",
+      "@SP",
+      "M=D",
+      Call.new("Sys.init", 0).to_asm
+    ]
+  end
 end
 
 asm = []
@@ -50,14 +60,6 @@ vm_files.each do |vm_file|
   clean_lines = raw_lines.map {|line| line.strip}
     .select {|line| !line.start_with?("//") && !line.empty?}
     .map {|line| line.split("//")[0].strip}
-
-    asm << [ # bootstrap code
-      "@256",
-      "D=A",
-      "@SP",
-      "M=D"
-    ]
-    asm << Call.new("Sys.init", 0).to_asm
 
     asm << Compiler.run(clean_lines, vm_file.split('/').last)
 end
