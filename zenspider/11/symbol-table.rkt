d522eb0e1a55a186d6f02e44cc6e3770229a25d9
Ryan Davis <ryand-ruby@zenspider.com>
2014-04-18 17:00:00 -0700

    Fix up symbol-table: no more default values for idx. Errors on new entries which helps keep function counts correct. Also fixed type vs scope on idx in env-add.

--- zenspider/11/symbol-table.rkt
+++ zenspider/11/symbol-table.rkt
@@ -6,17 +6,19 @@
 (struct var (type scope idx) #:transparent)
 
 (define (env-new)
-  (hash))
+  (hash "local"    0
+        "argument" 0
+        "static"   0))
 
 (define (env-set env key val)
   (hash-set env key val))
 
 (define (env-idx env scope)
-  (hash-ref env scope 0))
+  (hash-ref env scope))
 
 (define (env-add env key type scope)
   (define idx (env-idx env scope))
-  (env-set (env-set env type (add1 idx)) key (var type scope idx)))
+  (env-set (env-set env scope (add1 idx)) key (var type scope idx)))
 
 (define (env-get env key)
   (hash-ref env key))
